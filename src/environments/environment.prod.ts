cd5a5359ca9b3f74f8f51171d3f0a3f9f9d4666a
ARUNA SINGH <asingh@e2g.com>
2020-03-17 11:00:07 -0400

    Updated Enironment Prod File

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: '/messageboardapi.azurewebsites.net/MessageBoard',
+  paginationQueryString : '?PageIndex={0}&PageSize={1}',
+  width: "800px",
+  height: "500px"
 };
