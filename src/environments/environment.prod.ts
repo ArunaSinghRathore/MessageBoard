665546944953acd3d2e1bc5d698143d442b2232d
ARUNA SINGH <asingh@e2g.com>
2020-03-17 18:53:43 -0400

    config change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,16 @@
+// export const environment = {
+//   production: true,
+//   baseUrl: 'https://messageboardapi.azurewebsites.net/MessageBoard',
+//   paginationQueryString : '?PageIndex={0}&PageSize={1}',
+//   width: "800px",
+//   height: "500px",
+//   signalRUrl : 'https://messageboardapi.azurewebsites.net/Message'
+// };
 export const environment = {
   production: true,
-  baseUrl: 'https://messageboardapi.azurewebsites.net/MessageBoard',
+  baseUrl: 'https://localhost:44350/MessageBoard',
   paginationQueryString : '?PageIndex={0}&PageSize={1}',
   width: "800px",
   height: "500px",
-  signalRUrl : 'https://messageboardapi.azurewebsites.net/Message'
+  signalRUrl : 'https://localhost:44350/Message'
 };
