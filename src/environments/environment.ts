9572c8a9ddca910ff613ab420da736eea35b3366
ARUNA SINGH <asingh@e2g.com>
2020-03-18 12:42:03 -0400

    date issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-// export const environment = {
-//   production: false,
-//   baseUrl: 'https://localhost:44350/MessageBoard',
-//   paginationQueryString : '?PageIndex={0}&PageSize={1}',
-//   width: "800px",
-//   height: "500px",
-//   signalRUrl : 'https://localhost:44350/Message'
-// };
 export const environment = {
   production: true,
   baseUrl: 'https://messageboardapiv2.azurewebsites.net/MessageBoard',
@@ -18,11 +6,11 @@ export const environment = {
   height: "500px",
   signalRUrl : 'https://messageboardapiv2.azurewebsites.net/Message'
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// export const environment = {
+//   production: true,
+//   baseUrl: 'https://localhost:44350/MessageBoard',
+//   paginationQueryString : '?PageIndex={0}&PageSize={1}',
+//   width: "800px",
+//   height: "500px",
+//   signalRUrl : 'https://localhost:44350/Message'
+// };
